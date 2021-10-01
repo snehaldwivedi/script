ed6975062e8befd7838b0c7ad28313677291c583
Snehal Dwivedi <62747690+snehaldwivedi@users.noreply.github.com>
2021-10-01 10:16:56 +0530

    Updated move and wait for commands

--- fallback_install_task_command.ps1
+++ fallback_install_task_command.ps1
@@ -5,7 +5,7 @@ if(ERRORLEVEL -eq 1)
   Write-Warning "WARNING: Failed to install #{ChefUtils::Dist::Infra::PRODUCT} MSI package in remote context with status code ${MSIERRORCODE}."
   Write-Warning "WARNING: This may be due to a defect in operating system update KB2918614: http://support.microsoft.com/kb/2918614"
   $OLDLOGLOCATION="%CHEF_CLIENT_MSI_LOG_PATH%-fail.log"
-  # @move "%CHEF_CLIENT_MSI_LOG_PATH%" "!OLDLOGLOCATION!" > NUL
+  Move-Item -Path "{CHEF_CLIENT_MSI_LOG_PATH}" -Destination "{OLDLOGLOCATION}"
   Write-Warning WARNING: "Saving installation log of failure at ${OLDLOGLOCATION}"
   Write-Warning WARNING: "Retrying installation with local context..."
   # @schtasks /create /f  /sc once /st 00:00:00 /tn chefclientbootstraptask /ru SYSTEM /rl HIGHEST /tr \"cmd /c #{command} & sleep 2 & waitfor /s %computername% /si chefclientinstalldone\"
@@ -25,7 +25,7 @@ if(ERRORLEVEL -eq 1)
   {
     Write-Host "Successfully started #{ChefUtils::Dist::Infra::PRODUCT} installation scheduled task."
     Write-Host "Waiting for installation to complete -- this may take a few minutes..."
-    # waitfor chefclientinstalldone /t 600
+    waitfor chefclientinstalldone /t 600
     if(ERRORLEVEL -eq 1)
     {
       Write-Host "ERROR: Timed out waiting for #{ChefUtils::Dist::Infra::PRODUCT} package to install"
