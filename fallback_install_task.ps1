d1982458453bb3563681570e59ac9e3518b5716b
Snehal Dwivedi <62747690+snehaldwivedi@users.noreply.github.com>
2021-11-23 16:32:28 +0530

    Update fallback_install_task.ps1

--- fallback_install_task.ps1
+++ fallback_install_task.ps1
@@ -1,18 +1,17 @@
-$MSIERRORCODE= $LASTEXITCODE
-
-if($MSIERRORCODE -eq 1)
+if($LASTEXITCODE -eq 1)
 {
   Write-Warning "WARNING: Failed to install Chef Infra MSI package in remote context."
   Write-Warning "WARNING: This may be due to a defect in operating system update KB2918614: http://support.microsoft.com/kb/2918614"
+
   $OLDLOGLOCATION="%CHEF_CLIENT_MSI_LOG_PATH%-fail.log"
   Move-Item -Path "{CHEF_CLIENT_MSI_LOG_PATH}" -Destination "{OLDLOGLOCATION}"
+
   Write-Warning WARNING: "Saving installation log of failure at ${OLDLOGLOCATION}"
   Write-Warning WARNING: "Retrying installation with local context..."
-  # command = msiexec /qn /log '%CHEF_CLIENT_MSI_LOG_PATH%' /i '%LOCAL_DESTINATION_MSI_PATH%'
+
   try {
-    $command = #{command}
     New-Event -SourceIdentifier chefclientinstalldone -Sender $computername
-    $actions = (New-ScheduledTaskAction -Execute "$command"), (New-ScheduledTaskAction -Execute Start-Sleep 2)
+    $actions = (New-ScheduledTaskAction -Execute #{command}), (New-ScheduledTaskAction -Execute Start-Sleep 2)
     $trigger = New-ScheduledTaskTrigger -Once -At '0:00 AM'
     $principal = New-ScheduledTaskPrincipal -UserId 'SYSTEM' -RunLevel Highest
     $task = Register-ScheduledTask -TaskName "chefclientbootstraptask" -Trigger $trigger -Principal $principal -Action $actions
@@ -40,6 +39,7 @@ if($MSIERRORCODE -eq 1)
 
   Write-Host "Finished waiting for Chef Infra package to install."
   Unregister-ScheduledTask -TaskName 'chefclientbootstraptask' -Confirm:$false
-} else {
-  Write-Host "Successfully installed Chef Infra package."
+
+else{ 
+  Write-Host "Successfully installed #{ChefUtils::Dist::Infra::PRODUCT} package."
 }
